97c58a888778837e936070d3e27ed6dacb7fcbbc
Julian Lara <l.julian99_71@hotmail.com>
2021-03-21 14:13:02 -0500

    Commit inicial

---
+++ SerCompra/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SerCompra.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
