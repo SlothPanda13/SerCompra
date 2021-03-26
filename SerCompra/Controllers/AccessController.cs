24e05115aa67588e4c94c2856e1b62d10bafcf54
Julian Lara <l.julian99_71@hotmail.com>
2021-03-26 00:21:28 -0500

    Añadido funcionamiento al login
    Signed-off-by: Julian Lara <l.julian99_71@hotmail.com>

---
+++ SerCompra/Controllers/AccessController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SerCompra.Models.DataBase;
+using SerCompra.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace SerCompra.Controllers
+{
+    public class AccessController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Entrar(string email, string password)
+        {
+            try
+            {
+                using (var db = new sercompraContext())
+                {
+                    /*var usr = from d in db.Usuarios
+                        where d.Email == email && d.Contraseña == password
+                        select d;*/
+                    var rl = from d in db.Usuarios
+                        where (d.Email == email && d.Contraseña == password)
+                        select d.Rol;
+                    if (rl.Count() > 0)
+
+                    {
+                        if (rl.Contains("Administrador"))
+                        {
+                            return RedirectToAction("index", "Home");
+                        }
+                        if (rl.Contains("Funcionario"))
+                        {
+                            return RedirectToAction("index", "Home");
+                        }
+                        if (rl.Contains("Proveedor"))
+                        {
+                            return RedirectToAction("index", "Home");
+                        }
+
+                        
+                    }
+                    else {
+                        return Content("Usuario y/o contraseña invalidos");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return Content("Ocurrió un error " + e.Message);
+            }
+            //return View("Login");
+            return RedirectToAction("Privacy", "Home");
+
+        }
+    }
+}
---
+++ SerCompra/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerCompra.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email es requerido")]
+        [StringLength(45, ErrorMessage = "Logitud máxima 45")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Error Correo")]
+        [EmailAddress(ErrorMessage = "Correo electrónico incorrecto")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Contraseña requerida")]
+        [StringLength(45, ErrorMessage = "Logitud máxima 45")]
+        [DataType(DataType.Password, ErrorMessage = "Error contraseña")]
+        public string Contraseña { get; set; }
+    }
+}
